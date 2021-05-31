3b5eed55a9a863aeca5ffdcfc5b2490324169b5c
ambiernat <43310781+ambiernat@users.noreply.github.com>
2021-05-31 14:14:28 +0100

    Add files via upload

---
+++ 3_kindle_preproc.sql
@@ -0,0 +1,106 @@
+drop table if exists projectdb.kindle_all_v1;
+create table projectdb.kindle_all_v1
+-- filter only these entries for which asin is not null
+as
+with q1 as(
+ 	select 
+	a.overall,
+	a.vote,
+	a.verified,
+	a.reviewerid,
+	a.asin,
+	a.style,
+	a.reviewtext,
+	a.summary,
+	a.unixreviewtime
+	from kindle_all a 
+	where a.asin is not null
+)
+select * from q1;
+
+
+
+
+drop table if exists projectdb.kindle_all_v5;
+
+-- create table with books that appear at least 10 times in the dataset
+-- calculate mean review for each book as defined by asin
+-- filter only verified purchases entries to avoid unreliable reviews (just in case)
+-- caluclate mean number of reviews across a certain book as defined by asin
+-- move text reviews from 'vote' column to 'reviewtext' column if there is no text in the 'reviewcolumn'
+
+create table projectdb.kindle_all_v5
+as
+with q4 as(
+  
+  SELECT 
+		a.overall,
+		a.vote,
+		a.verified,
+		a.reviewerid,
+		a.style,
+		case when a.reviewtext is null and LENGTH(a.vote)>3 then a.vote 
+  			else a.reviewtext 
+  		end as reviewtext,
+		a.summary,
+		a.unixreviewtime,
+  		a.asin,
+  		a.count_asin,
+  		a.mean_review,
+		AVG(a.count_asin) OVER (PARTITION BY a.asin) as mean_no_reviews
+  FROM q3 a  
+),
+
+q3 as(
+  
+  SELECT 
+		a.overall,
+		a.vote,
+		a.verified,
+		a.reviewerid,
+		a.style,
+		a.reviewtext,
+		a.summary,
+		a.unixreviewtime,
+  		a.asin,
+  		a.count_asin,
+  		a.mean_review,
+		AVG(a.count_asin) OVER (PARTITION BY a.asin) as mean_no_reviews
+  FROM q2 a  
+),
+
+q2 as(
+  
+  SELECT 
+		a.overall,
+		a.vote,
+		a.verified,
+		a.reviewerid,
+		a.style,
+		a.reviewtext,
+		a.summary,
+		a.unixreviewtime,
+  		a.asin,
+		COUNT(a.asin) OVER (PARTITION BY a.asin) as count_asin,
+		AVG(a.overall) OVER (PARTITION BY a.asin) as mean_review
+  FROM q1 a  
+),
+
+q1 as(
+  
+  SELECT 
+		a.overall,
+		a.vote,
+		a.verified,
+		a.reviewerid,
+		a.style,
+		a.reviewtext,
+		a.summary,
+		a.unixreviewtime,
+  		a.asin
+  FROM projectdb.kindle_all_v1 a  
+  where a.verified = true
+)
+
+select * from q4
+where count_asin >= 10
